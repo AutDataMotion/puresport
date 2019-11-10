63fba15170e312b88138fda69109a89f2d34d537
zhuchaobin <nanjing2007@163.com>
2019-11-10 15:49:43 +0800

    上线sql脚本

---
+++ doc/数据库/二期/投产脚本/alter_t5_t7_t9_10_t11_t12_t1.sql
@@ -0,0 +1,13 @@
+ALTER TABLE t5_crcl_stdy add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE t5_crcl_stdy add COLUMN  `category` char(2) DEFAULT '' COMMENT '科目';
+ALTER TABLE t7_crcl add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE t7_crcl add COLUMN  `category` char(2) DEFAULT '' COMMENT '科目';
+ALTER TABLE t9_tstlib add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE t9_tstlib add COLUMN  `category` char(2) DEFAULT '' COMMENT '科目';
+ALTER TABLE t10_exam_grd add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE t10_exam_grd add COLUMN  `category` char(2) DEFAULT '' COMMENT '科目';
+ALTER TABLE t11_exam_stat add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE t11_exam_stat add COLUMN  `category` char(2) DEFAULT '' COMMENT '科目';
+ALTER TABLE t12_highest_score add COLUMN type char(2) DEFAULT '' COMMENT '赛事类型';
+ALTER TABLE `t1_usr_bsc` ADD COLUMN `email_val` INT(11) NULL DEFAULT 0 COMMENT '邮箱是否验证' AFTER `levelinstitute`;
+ALTER TABLE `t1_usr_bsc` ADD COLUMN `mblph_val` INT(11) NULL DEFAULT 0 COMMENT '手机是否验证' AFTER `email_val`;
